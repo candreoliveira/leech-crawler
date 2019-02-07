3a11be9df50868d4c815ef518ceb87c0c077ff68
Carlos Oliveira <carlos.baptista@b2wdigital.com>
2019-02-07 17:02:31 -0200

    fix(headless): Fixing merge.

--- src/parser/html/headless.mjs
+++ src/parser/html/headless.mjs
@@ -63,14 +63,8 @@ class Headless extends Parser {
         "--disable-setuid-sandbox",
         "--ignore-certificate-errors"
       ],
-<<<<<<< HEAD
-      headless: false,
-      devtools: true,
-=======
-      executablePath: '/usr/bin/chromium-browser',
       headless: true,
       devtools: false,
->>>>>>> 4096d80... remove alfred
       obeyRobotsTxt: false,
       maxConnections: 20,
       userAgent: userAgent("rotate", this.args.website),
